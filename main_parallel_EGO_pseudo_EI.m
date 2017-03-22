360261630bf380d451f2cf8171b9277399d0f6c7
Dawei Zhan <zhandawei@hust.edu.cn>
2017-03-22 21:34:03 +0800

    update

--- main_parallel_EGO_pseudo_EI.m
+++ main_parallel_EGO_pseudo_EI.m
@@ -1,21 +1,20 @@
 %--------------------------------------------------------------------------
 % The parallel Efficient Global Optimization (EGO) algorithm [1] using the
 % pseudo Expected Improvement criterion (PEI) [2].
-%
 % the DACE toolbox of  Lophaven et al. (2002) [3] is used to build the kriging model
 %--------------------------------------------------------------------------
 % Reference:
-% Jones, D.R., Schonlau, M., Welch, W.J.: Efficient global optimization of
+% [1] Jones, D.R., Schonlau, M., Welch, W.J.: Efficient global optimization of
 % expensive black-box functions. Journal of Global Optimization 13(4),
 % 455-492 (1998).
 %
-%Lophaven SN, Nielsen HB, and Sodergaard J, DACE - A MATLAB Kriging
-%Toolbox, Technical Report IMM-TR-2002-12, Informatics and Mathematical
-%Modelling, Technical University of Denmark, 2002.
-%Available at: http://www2.imm.dtu.dk/~hbn/dace/.
-%
-% D. Zhan, J. Qian, Y. Cheng, Pseudo expected improvement criterion for
+% [2] D. Zhan, J. Qian, Y. Cheng, Pseudo expected improvement criterion for
 % parallel, Journal of Global Optimization. doi:10.1007/s10898-016-0484-7
+%
+% [3]Lophaven SN, Nielsen HB, and Sodergaard J, DACE - A MATLAB Kriging
+% Toolbox, Technical Report IMM-TR-2002-12, Informatics and Mathematical
+% Modelling, Technical University of Denmark, 2002.
+% Available at: http://www2.imm.dtu.dk/~hbn/dace/.
 %--------------------------------------------------------------------------
 % Zhan Dawei (zhandawei@hust.edu.cn)
 % This program is free software; you can redistribute it and/or
@@ -25,27 +24,27 @@
 %--------------------------------------------------------------------------
 clearvars;
 addpath('dace');
+addpath('test_problem')
 %--------------------------------------------------------------------------
 % setting of the problem
 fun_name = 'Sixhump';
-design_space = [-2,-2;2,2];
 % the number of initial design points
-num_initial_sample = 20;
+num_initial_sample = 50;
 % the number of total allowed design points
-num_total_sample = 58;
+num_total_sample = 100;
 % the number of points selected in each iteration (cycle)
 num_select = 4;
-% number of design value
-num_vari = size(design_space,2);
-
 %--------------------------------------------------------------------------
 % settings of the genetic algorithm
 ga_population_size = 100;
 ga_generation = 100;
 ga_crossover_fraction = 0.9;
 ga_option = gaoptimset( 'PopulationSize',ga_population_size, 'Generations',ga_generation,...
-                                                   'StallGenLimit',ga_generation, 'CrossoverFraction',0.9, 'Display', 'off');
-                                            
+                                                   'StallGenLimit',ga_generation, 'CrossoverFraction',ga_crossover_fraction, 'Display', 'off');
+    
+ %--------------------------------------------------------------------------
+ % get the information of the test problem
+[num_vari,design_space,optimum] = Test_Function(fun_name);                                               
 %--------------------------------------------------------------------------
 % the 0th iteration
 % initial design points using Latin hypercube sampling method
@@ -60,7 +59,7 @@ Kriging_model = dacefit(sample_x,sample_y,'regpoly0','corrgauss',1*ones(1,num_va
 iteration = 0;
 evaluation = size(sample_x,1);
 % print the current information to the screen
-fprintf(' iteration: %d, evaluation: %d, current best solution: %f\n', iteration, evaluation, f_min);
+fprintf(' iteration: %d, evaluation: %d, current best solution: %f: real optimum: %f\n', iteration, evaluation, f_min, optimum);
 
 %--------------------------------------------------------------------------
 % the iteration
@@ -99,7 +98,7 @@ while evaluation < num_total_sample
     evaluation = size(sample_x,1);
     f_min = min(sample_y);
     % print the current information to the screen
-    fprintf(' iteration: %d, evaluation: %d, current best solution: %f\n', iteration, evaluation, f_min);
+    fprintf(' iteration: %d, evaluation: %d, current best solution: %f: real optimum: %f\n', iteration, evaluation, f_min, optimum);
     
 end
 
